57dcfe1353f0628797999dfcfed3641470cf04f1
pythenz <83879960+pythenz@users.noreply.github.com>
2025-10-15 21:20:22 -0400

    Delete Apps/Cogs directory

--- Apps/Cogs/embeds.py
+++
@@ -1,8 +0,0 @@
-from nextcord.ext import commands
-
-class Embeds(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-def setup(bot):
-    bot.add_cog(Embeds(bot))
--- Apps/Cogs/moderation.py
+++
@@ -1,8 +0,0 @@
-from nextcord.ext import commands
-
-class Moderation(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-def setup(bot):
-    bot.add_cog(Moderation(bot))
